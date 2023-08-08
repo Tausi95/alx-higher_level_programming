9119e26643db9290a66062a320299d67d9cd5171
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-08-08 22:11:36 +0200

    fixed bugs

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if (x < 99):
-        print("{:d}".format((i // 10), (x % 10)), end=", ")
+    if (i < 99):
+        print("{:d}".format((i // 10), (i % 10)), end=", ")
     else:
-        print("{:d}{:d}".format((x // 10), (x % 10)))
+        print("{:d}{:d}".format((i // 10), (i % 10)))
