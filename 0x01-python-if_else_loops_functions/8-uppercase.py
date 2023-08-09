157d58ea1f41a6025df666a70f204d0566db0dbb
Chancy Tausi Tsonga <effortbuildingcontractors@gmail.com>
2023-08-09 09:21:01 +0200

    fixed identation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if(65 <= ord(i) <= 122):
-            if(97 <= ord(i) <= 122):
+        if 65 <= ord(i) <= 122:
+            if 97 <= ord(i) <= 122:
                 i = chr(ord(i) - 32)
         print("{:s}".format(i), end="")
     print("")
